97a925a1f0cd6d5468ba54118d33f0ba961d388d
RaySHAOWEI <45119387+RaySHAOWEI@users.noreply.github.com>
2023-04-04 16:38:19 +0800

    完善初始发布接收端，编写所需节点 (#4)
    sub.txt文件中的话题不是output，和作业所需要求不符，pub.txt文件中发布间隔太长，代码冗长。

---
+++ homework/话题通信/code/src/sub.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void doMsg(const std_msgs::String::ConstPtr& msg_p){
+    ROS_INFO("接收端:%s",msg_p->data.c_str());//调试输出
+}
+int main(int argc, char  *argv[])
+{
+    /* 
+        话题名称：output_topic
+        话题内容：收到的被处理过后的大写字母
+        消息类型：std_msgs::string
+    */
+    setlocale(LC_ALL,"");
+    //初始化 ROS 节点
+    ros::init(argc,argv,"listener");
+    //实例化 ROS 句柄
+    ros::NodeHandle nh;
+
+    //实例化 订阅者 对象
+    ros::Subscriber sub = nh.subscribe<std_msgs::String>("output_topic",10,doMsg);
+    //处理订阅的消息(回调函数)
+
+    //设置循环调用回调函数
+    ros::spin();//循环读取接收的数据，并调用回调函数处理
+
+    return 0;
+}
