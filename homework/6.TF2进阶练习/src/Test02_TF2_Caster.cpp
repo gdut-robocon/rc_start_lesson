4ff6b13993462f195c24e8365fe148d8f58e853f
Jayan109 <877047884@qq.com>
2023-05-27 18:07:36 +0800

    homework6

---
+++ homework/6.TF2进阶练习/src/Test02_TF2_Caster.cpp
@@ -0,0 +1,78 @@
+/*  
+    该文件实现:需要订阅 turtle1 和 turtle2 的 pose，然后广播相对 world 的坐标系信息
+
+    注意: 订阅的两只 turtle,除了命名空间(turtle1 和 turtle2)不同外,
+          其他的话题名称和实现逻辑都是一样的，
+          所以我们可以将所需的命名空间通过 args 动态传入
+
+    实现流程:
+        1.包含头文件
+        2.初始化 ros 节点
+        3.解析传入的命名空间
+        4.创建 ros 句柄
+        5.创建订阅对象
+        6.回调函数处理订阅的 pose 信息
+            6-1.创建 TF 广播器
+            6-2.将 pose 信息转换成 TransFormStamped
+            6-3.发布
+        7.spin
+
+*/
+//1.包含头文件
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "geometry_msgs/TransformStamped.h"
+//保存乌龟名称
+std::string turtle_name;
+
+
+void doPose(const turtlesim::Pose::ConstPtr& pose){
+    //  6-1.创建 TF 广播器 ---------------------------------------- 注意 static
+    static tf2_ros::TransformBroadcaster broadcaster;
+    //  6-2.将 pose 信息转换成 TransFormStamped
+    geometry_msgs::TransformStamped tfs;
+    tfs.header.frame_id = "world";
+    tfs.header.stamp = ros::Time::now();
+    tfs.child_frame_id = turtle_name;
+    tfs.transform.translation.x = pose->x;
+    tfs.transform.translation.y = pose->y;
+    tfs.transform.translation.z = 0.0;
+    tf2::Quaternion qtn;
+    qtn.setRPY(0,0,pose->theta);
+    tfs.transform.rotation.x = qtn.getX();
+    tfs.transform.rotation.y = qtn.getY();
+    tfs.transform.rotation.z = qtn.getZ();
+    tfs.transform.rotation.w = qtn.getW();
+    //  6-3.发布
+    broadcaster.sendTransform(tfs);
+
+} 
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL,"");
+    // 2.初始化 ros 节点
+    ros::init(argc,argv,"pub_tf");
+    // 3.解析传入的命名空间
+    if (argc != 2)
+    {
+        ROS_ERROR("请传入正确的参数");
+    } else {
+        turtle_name = argv[1];
+        ROS_INFO("乌龟 %s 坐标发送启动",turtle_name.c_str());
+    }
+
+    // 4.创建 ros 句柄
+    ros::NodeHandle nh;
+    // 5.创建订阅对象
+    ros::Subscriber sub = nh.subscribe<turtlesim::Pose>(turtle_name + "/pose",1000,doPose);
+    //     6.回调函数处理订阅的 pose 信息
+    //         6-1.创建 TF 广播器
+    //         6-2.将 pose 信息转换成 TransFormStamped
+    //         6-3.发布
+    // 7.spin
+    ros::spin();
+    return 0;
+}
